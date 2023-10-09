73793b39bf9ac8457d87ecae9ef96cbd93916f81
Kim6-9 <you@example.com>
2023-10-09 14:38:26 -0700

    my commit

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void){
+	int password = 20;
+		if (password < 500){
+			printf("yeah %d is right \n" ,password);
+	printf("welcom \n");
+		}
+		else {
+			printf("no you r not");
+		}
+
+
+		return (0);
+}
