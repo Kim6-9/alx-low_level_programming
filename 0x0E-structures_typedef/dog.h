6858a06655d3304131aa9c45c730d67af870a669
Kim6-9 <you@example.com>
2023-11-06 11:50:04 -0800

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog
- * @name of dog
- * @age of dog
- * @owner of dog 
+ * struct dog- dog strucr
+ * @name :name of dog
+ * @age :age of dog
+ * @owner:owner of dog
+ *
+ * descreption : just a long dog struct in a kiyyy world
  **/
 struct dog
 {
@@ -12,5 +14,6 @@ struct dog
       	float age;
 	char *owner;
 };
+
 #endif
 
